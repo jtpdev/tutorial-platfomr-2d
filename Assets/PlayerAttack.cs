f7a45223ebb0b1c65cda023095cffb2afae13ba7
Jimmy Porto <ireturnerd@gmail.com>
2018-07-27 13:11:05 -0300

    Finished 3th movie

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public float intervaloDeAtaque;
+    private float proximoAtaque;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = gameObject.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1") && Time.time > proximoAtaque)
+        {
+            Atacando();
+        }
+		
+	}
+
+    void Atacando()
+    {
+        anim.SetTrigger("Ataque");
+        proximoAtaque = Time.time + intervaloDeAtaque;
+    }
+}
